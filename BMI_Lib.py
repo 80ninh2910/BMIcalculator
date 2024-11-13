24316a6bc1bb753153a3087b9174bbec56fc8310
QuocViett-ec <viethq234112e@st.uel.edu.vn>
2024-11-13 12:40:51 +0700

    Create BMI_Lib.py

---
+++ BMI_Lib.py
@@ -0,0 +1,11 @@
+import math
+def CalBMI(height,weight):
+    BMI=weight/(height*height)
+    if BMI<18.5:
+        return "thin"
+    elif 18.5<=BMI<25:
+        return "normal"
+    elif 25<=BMI<30:
+        return "fat"
+    else:
+        return "obesity"
