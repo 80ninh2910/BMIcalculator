554dab25397a1012c4d35bade1cb587cbf80ca7f
QuocViett-ec <viethq234112e@st.uel.edu.vn>
2024-11-13 14:01:43 +0700

    Update BMI_Lib.py

--- ui/MainWindowExt.py
+++ ui/MainWindowExt.py
@@ -7,11 +7,13 @@ class MainWindowExt(Ui_MainWindow):
         super().setupUi(MainWindow)
         self.MainWindow = MainWindow
         self.setupSignalsAndSlots()
+        self.pushButtonRe.clicked.connect(self.process_clear)
 
 
 
     def setupSignalsAndSlots(self):
         self.pushButtoncal.clicked.connect(self.Cal)
+
     def Cal(self):
         height=float(self.lineEditheight.text())
         weight=float(self.lineEditweight.text())
@@ -20,4 +22,13 @@ class MainWindowExt(Ui_MainWindow):
         self.lineEditBMI.setText(f"{bmi}")
         self.lineEditStatus.setText(f"{status}")
 
+    def process_clear(self):
+        self.lineEditweight.setText("")
+        self.lineEditheight.setText("")
+        self.lineEditStatus.setText("")
+        self.lineEditBMI.setText("")
+        self.lineEditweight.setFocus()
+
+    def show(self):
+        self.MainWindow.show()
 
