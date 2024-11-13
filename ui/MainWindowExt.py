85aeda488bbd56aa280b4129107258fcb96560e7
tuantubatpham <doanquocvinhcute7@gmail.com>
2024-11-13 13:23:20 +0700

    up lần 3

---
+++ ui/MainWindowExt.py
@@ -0,0 +1,14 @@
+from ui.MainWindow import Ui_MainWindow
+
+
+class MainWindowExt(Ui_MainWindow):
+    def setupUi(self, MainWindow):
+        super().setupUi(MainWindow)
+        self.MainWindow = MainWindow
+        self.setupSignalsAndSlots()
+
+    def showWindow(self):
+        self.MainWindow.show()
+
+    def setupSignalsAndSlots(self):
+        pass
