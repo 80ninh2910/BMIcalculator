1fef4cd3ba7edbbd370e5c7e93db651b1d18ef11
80ninh2910 <ninhqa234112e@st.uel.edu.vn>
2024-11-14 09:39:21 +0700

    Delete testcase.py

--- testcase.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-
-from ui.MainWindowExt import MainWindowExt
-
-app=QApplication([])
-mywindow=MainWindowExt()
-mywindow.setupUi(QMainWindow())
-mywindow.show()
-app.exec()
